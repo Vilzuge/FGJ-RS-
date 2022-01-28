d46afb6f35c76fe43d1e370f3baaea765a275086
AttePerttola <atte.perttola@gmail.com>
2022-01-29 00:34:25 +0200

    speed powerup v2

--- Assets/Scripts/SpeedPowerup.cs
+++ Assets/Scripts/SpeedPowerup.cs
@@ -15,6 +15,7 @@ public class SpeedPowerup : MonoBehaviour
             if (movementScript)
             {
                 movementScript.moveSpeed += increase;
+                Destroy(gameObject);
             }
         }
     }
