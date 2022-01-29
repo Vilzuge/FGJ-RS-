6d1482723d69aca1dc7b229c1d1bbee45d8dcc37
otsoweckstrom <42272286+otsoweckstrom@users.noreply.github.com>
2022-01-29 19:55:59 +0200

    jump mechanic

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,44 +12,70 @@ public class PlayerMovement : MonoBehaviour
     float movement;
     public Turn turn;
 
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+    private int extraJumps;
+    public int extraJumpsValue;
+
     private void Start()
     {
+        extraJumps = extraJumpsValue;
         turn = GetComponent<Turn>();
     }
 
     void Update()
     {
+
+        if (isGrounded == true)
+        {
+            extraJumps = extraJumpsValue;
+        }
+
+
         if (turn.GetCurrentTurn() == TurnState.Dark)
         {
             rb.gravityScale = 5f;
-            jumpForce = 15;
+            jumpForce = 20;
             movement = Input.GetAxis("Horizontal");
             if (Input.GetButtonDown("Jump"))
             {
                 Jump();
             }
         }
-        
+
         if (turn.GetCurrentTurn() == TurnState.Bright)
         {
             rb.gravityScale = -5f;
-            jumpForce = -15;
+            jumpForce = -20;
             movement = Input.GetAxis("Horizontal");
             if (Input.GetButtonDown("Jump"))
             {
                 Jump();
             }
+
         }
-        
+
     }
 
     void FixedUpdate()
     {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
         rb.velocity = new Vector2(movement * moveSpeed, rb.velocity.y);
     }
 
     void Jump()
     {
-        rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        if (extraJumps > 0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }
+        else if (extraJumps == 0 && isGrounded == true)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+
     }
 }
